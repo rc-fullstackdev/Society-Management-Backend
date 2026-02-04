53ca301cd9e12a82344569c7813c1da3da6b48d9
ravib7 <ravibchaudhari7@gmail.com>
2026-02-04 22:46:09 +0530

    frontend live url added

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ require("dotenv").config()
 const app = express()
 
 app.use(express.json())
-app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+// app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+app.use(cors({ origin: "https://society-management-frontend-one.vercel.app", credentials: true }))
 app.use(cookieParser())
 
 app.use("/api/auth", require("./routes/auth.route"))
